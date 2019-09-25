08039da6f6afafc799a1c2e4a5e1ab61f22e8018
Octavian Taranu <taranu.toma.octavian@gmail.com>
2019-09-25 16:37:09 +0100

    using www file

--- server.js
+++ server.js
@@ -1,17 +1,19 @@
 var path = require('path');
 var express = require('express'),
-  app = express(),
+  server = express(),
   port = process.env.PORT || 3200;
   Task = require('./api/models/apiModel'),
   bodyParser = require('body-parser');
 
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
 
 var routes = require('./api/routes/apiRoutes');
-routes(app);
-app.use('/checkpoints', express.static(path.join(__dirname, 'api/checkpoints')));
+routes(server);
+server.use('/checkpoints', express.static(path.join(__dirname, 'api/checkpoints')));
 
-app.listen(port);
+server.listen(port);
+
+module.exports = server;
 
 console.log('BitsOfBeats API server started on: ' + port);
